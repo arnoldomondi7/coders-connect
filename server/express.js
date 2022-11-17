0cdacde4f0ed619a18b9e2f69b881cdddef8f32a
Arnold Omondi <arnoldomondi7@gmail.com>
2022-11-18 00:17:09 +0300

    statrted working on the server

---
+++ server/express.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
+import compression from 'compression'
+import cors from 'cors'
+
+//get the morgan.
+const morgan = require('morgan')
+
+//initiate the app.
+const app = express()
+
+//initiate the middlewares.
+app.use(express.json())
+app.use(cookieParser())
+app.use(helmet())
+app.use(compression())
+app.use(cors())
+app, use(morgan('dev'))
+
+//export the app.
+export default app
