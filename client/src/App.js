7c7a799040e5892fb533f2469fc15ea6e0d19ea9
Arnold Omondi <arnoldomondi7@gmail.com>
2022-11-23 01:50:35 +0300

    started work on the front-end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainRouter from './MainRouter'
+
+
+function App() {
+  return (
+    <div >
+      <MainRouter />
+    </div>
+  )
+}
+
+export default App
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,18 +1,33 @@
 import express from 'express'
 
-
 const router = express.Router()
 
 //data from the post controlelr.
-import { createPost } from '../controllers/post.controller'
+import {
+    createPost,
+    deletePost,
+    getPost,
+    likePost,
+    timeline,
+    updatePost
+} from '../controllers/post.controller'
 
 //create post
 router.post('/post', createPost)
+
 //get post.
+router.get('/post/:id', getPost)
 //update post.
+router.put('/likes/:id', likePost)
+
 //like a post.
+router.put('/update/:id', updatePost)
+
 //get timeline post.
+router.get('/timeline/all', timeline)
+
 //delete a post
+router.delete('/delete/:id', deletePost)
 
 //export the router.
 module.exports = router
